4e374e22a2536b31dd5910dd3f00348eb5cfbfe9
Conor Storey <conorwstorey@gmail.com>
2024-03-27 20:59:58 +0000

    link prefetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,7 +90,7 @@ function SocialLink({
   icon: React.ComponentType<{ className?: string }>
 }) {
   return (
-    <Link className="group -m-1 p-1" {...props}>
+    <Link className="group -m-1 p-1" {...props} prefetch={true}>
       <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
     </Link>
   )
