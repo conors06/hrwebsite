467f16fe0edb88b899823d75659ecd75a830222d
Conor Storey <conorwstorey@gmail.com>
2024-03-27 14:18:56 +0000

    new pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -133,9 +133,9 @@ function MobileNavigation(
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
                 <NavItem href="/">Home</NavItem>
-                <NavItem href="/speaking">HR Services</NavItem>
+                <NavItem href="/services">HR Services</NavItem>
                 <NavItem href="/testimonials">Testimonials</NavItem>
-                <NavItem href="/projects">Contact us</NavItem>
+                <NavItem href="/contact">Contact us</NavItem>
               </ul>
             </nav>
           </Popover.Panel>
@@ -179,9 +179,9 @@ function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/">Home</NavItem>
-        <NavItem href="/speaking">HR Services</NavItem>
+        <NavItem href="/services">HR Services</NavItem>
         <NavItem href="/testimonials">Testimonials</NavItem>
-        <NavItem href="/projects">Contact us</NavItem>
+        <NavItem href="/contact">Contact us</NavItem>
       </ul>
     </nav>
   )
@@ -435,7 +435,7 @@ export function Header() {
                   </AvatarContainer>
                 )}
               </div>
-              <div className="flex  justify-end md:justify-center">
+              <div className="flex justify-end md:justify-center">
                 <MobileNavigation className="pointer-events-auto md:hidden" />
                 <DesktopNavigation className="pointer-events-auto hidden md:block" />
               </div>
