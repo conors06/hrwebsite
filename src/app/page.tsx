bf4603cac45a1dc9ca9008287904d87bc6bee974
Conor Storey <conorwstorey@gmail.com>
2024-04-03 22:35:14 +0100

    start of testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default async function Home() {
   return (
     <>
       <Container className="mt-9">
-        <div className="max-w-2xl">
+        <div className="max-w-full">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
             Walshe HR
           </h1>
-          <div className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+          <div className="mt-6 text-base text-center text-zinc-600 dark:text-zinc-400">
             <TextGenerateEffectDemo />
           </div>
         </div>
--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -1,24 +1,23 @@
 import { Container } from '@/components/Container'
 import Image from 'next/image';
+import sjogLogo from '@/images/logos/sjog.jpg';
 
 const featuredTestimonial = {
-    body: 'Integer id nunc sit semper purus. Bibendum at lacus ut arcu blandit montes vitae auctor libero. Hac condimentum dignissim nibh vulputate ut nunc. Amet nibh orci mi venenatis blandit vel et proin. Non hendrerit in vel ac diam.',
+    body: 'Anna Marie is the person you want to meet when you require HR support services. Anna Marie fosters a healthy work environment that attracts and inspires excellence in people. Anna Marie is not a custodian of the HR Policies but can implement the policies in a way that informs, and promotes feedback and real staff engagement which is very difficult to do. Anna Marie does this naturally, she has true integrity and her knowledge across the broad sector of HR is a real loss to our organisation but a major benefit to those who work with Anna Marie now and in the future.',
     author: {
-        name: 'Brenna Goyette',
-        handle: 'brennagoyette',
-        imageUrl:
-            'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=1024&h=1024&q=80',
-        logoUrl: 'https://tailwindui.com/img/logos/savvycal-logo-gray-900.svg',
+        name: 'Natalie Sheils',
+        handle: 'Operations Manager, St John of God Community Services',
+
     },
 }
 const testimonials = [
     [
         [
             {
-                body: 'Laborum quis quam. Dolorum et ut quod quia. Voluptas numquam delectus nihil. Aut enim doloremque et ipsam.',
+                body: 'I have been more than happy with the quality of Anna Marie’s work. Anna Marie is an absolute professional, nothing phases her. In addition to having a very impressive HR knowledge-base, Anna Marie is pragmatic, cogent and very personally engaging, putting people at their ease, something which is invaluable when having potentially difficult conversations.',
                 author: {
-                    name: 'Leslie Alexander',
-                    handle: 'lesliealexander',
+                    name: 'James Forbes',
+                    handle: 'Director of Care, ChildVision, National Education Centre for Blind Children',
                     imageUrl:
                         'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
                 },
@@ -32,7 +31,7 @@ const testimonials = [
                     name: 'Lindsay Walton',
                     handle: 'lindsaywalton',
                     imageUrl:
-                        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
                 },
             },
             // More testimonials...
@@ -46,7 +45,7 @@ const testimonials = [
                     name: 'Tom Cook',
                     handle: 'tomcook',
                     imageUrl:
-                        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
                 },
             },
             // More testimonials...
@@ -58,7 +57,7 @@ const testimonials = [
                     name: 'Leonard Krasner',
                     handle: 'leonardkrasner',
                     imageUrl:
-                        'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+                        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
                 },
             },
             // More testimonials...
@@ -101,9 +100,8 @@ export default function Testimonials() {
                 </div>
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto max-w-xl text-center">
-                        <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Testimonials</h2>
                         <p className="mt-2 text-3xl dark:text-zinc-100 font-bold tracking-tight text-gray-900 sm:text-4xl">
-                            I have worked with thousands of amazing people
+                            Testimonials
                         </p>
                     </div>
                     <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
@@ -112,18 +110,11 @@ export default function Testimonials() {
                                 <p>{`“${featuredTestimonial.body}”`}</p>
                             </blockquote>
                             <figcaption className="flex flex-wrap items-center gap-x-4 gap-y-4 border-t border-gray-900/10 px-6 py-4 sm:flex-nowrap">
-                                <Image
-                                    className="h-10 w-10 flex-none rounded-full bg-gray-50"
-                                    src={featuredTestimonial.author.imageUrl}
-                                    alt=""
-                                    width={40}
-                                    height={40}
-                                />
+                                <Image className="h-10 w-auto flex-none" src={sjogLogo} alt="St John of God Logo" width={40} height={10} />
                                 <div className="flex-auto">
                                     <div className="font-semibold">{featuredTestimonial.author.name}</div>
-                                    <div className="text-gray-600">{`@${featuredTestimonial.author.handle}`}</div>
+                                    <div className="text-gray-600">{`${featuredTestimonial.author.handle}`}</div>
                                 </div>
-                                <Image className="h-10 w-auto flex-none" src={featuredTestimonial.author.logoUrl} alt="" width={40} height={10} />
                             </figcaption>
                         </figure>
                         {testimonials.map((columnGroup, columnGroupIdx) => (
@@ -151,7 +142,7 @@ export default function Testimonials() {
                                                     <Image className="h-10 w-10 rounded-full bg-gray-50" src={testimonial.author.imageUrl} alt="" width={40} height={40} />
                                                     <div>
                                                         <div className="font-semibold">{testimonial.author.name}</div>
-                                                        <div className="text-gray-600">{`@${testimonial.author.handle}`}</div>
+                                                        <div className="text-gray-600">{`${testimonial.author.handle}`}</div>
                                                     </div>
                                                 </figcaption>
                                             </figure>
