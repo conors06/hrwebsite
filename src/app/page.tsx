dac2a29d61a27cf1fa846f4405b210e85261f693
Conor Storey <conorwstorey@gmail.com>
2024-04-05 21:10:10 +0100

    removed hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,16 +17,6 @@ export default async function Home() {
           </div>
         </div>
       </Container>
-      <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="flex flex-col gap-16">
-            hello
-          </div>
-          <div className="space-y-10 lg:pl-16 xl:pl-24">
-
-          </div>
-        </div>
-      </Container>
     </>
   )
 }
