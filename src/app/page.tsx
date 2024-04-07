8a38d4db3af0b226ad33c4282abf48a9ff483f2d
Conor Storey <conorwstorey@gmail.com>
2024-04-07 21:05:11 +0100

    home sparkles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,40 @@
 import { Container } from '@/components/Container'
-import { TextGenerateEffectDemo } from '@/components/text-generate-demo';
+import { SparklesCore } from "@/components/sparkles";
 
 
-export default async function Home() {
-
+export default function Home() {
   return (
     <>
+
       <Container className="mt-9">
-        <div className="max-w-full">
-          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Walshe HR
-          </h1>
-          <div className="mt-6 text-xl text-center text-zinc-600 dark:text-zinc-400">
-            I’m Anna Marie, a HR Professional with more than 18 years experience working in
+          <div className="max-w-full">
+            <h1 className="text-4xl text-center font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+              Walshe HR
+            </h1>
+            <div className="w-[20rem] sm:w-[40rem] mt-3 h-40 mx-auto relative">
+              {/* Gradients */}
+              <div className="absolute inset-x-0 top-0 bg-gradient-to-r from-transparent via-rose-100 to-transparent h-[2px] w-3/4 blur-sm mx-auto" />
+              <div className="absolute inset-x-0 top-0 bg-gradient-to-r from-transparent via-rose-100 to-transparent h-px w-3/4 mx-auto" />
+              <div className="absolute inset-x-0 top-0 bg-gradient-to-r from-transparent via-fuchsia-500 to-transparent h-[5px] w-1/4 blur-sm mx-auto" />
+              <div className="absolute inset-x-0 top-0 bg-gradient-to-r from-transparent via-fuchsia-500 to-transparent h-px w-1/4 mx-auto" />
+
+              {/* Core component */}
+              <SparklesCore
+                background="transparent"
+                minSize={0.4}
+                maxSize={1}
+                particleDensity={1200}
+                className="w-full h-full mx-auto"
+                particleColor="#c989cc"
+              />
+
+              {/* Radial Gradient to prevent sharp edges */}
+              <div className="absolute inset-0 w-full h-full bg-white [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+
+            </div>
+          </div>
+          <div className="mt-12 text-xl text-center text-zinc-600 dark:text-zinc-100">
+            I&apos;m Anna Marie, a HR Professional with more than 18 years experience working in
             a variety of sectors, for example Healthcare, Not for Profit,
             Banking and Technology.
             Knowledgeable in all areas of HR with particular expertise in
@@ -24,8 +46,8 @@ export default async function Home() {
             Passionate about people and the value that the Human
             Resources function can bring to how people experience work.
           </div>
-        </div>
       </Container>
+
     </>
   )
 }
