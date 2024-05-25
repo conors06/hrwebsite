834e2baa1cad8d636c029360baee3983e40848a0
Conor Storey <conorwstorey@gmail.com>
2024-05-25 21:24:48 +0100

    chore: Update dependencies and remove unused components in page.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,11 @@
-import MillionLint from '@million/lint'
-import rehypePrism from '@mapbox/rehype-prism'
-import remarkGfm from 'remark-gfm'
+// @ts-check
 
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
     reactStrictMode: true,
     pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
 }
 
-/** @type {import('next').NextConfig} */
-export default MillionLint.next({
-    rsc: true,
-})(
-    MillionLint.next(nextConfig, {
-        rsc: true,
-    })
-)
+export default nextConfig
